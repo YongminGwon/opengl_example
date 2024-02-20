bdaa286d2a73d8fc9302eeaf92f6c50548d465b6
arsen <clumsyrhymer@gmail.com>
2024-02-20 22:20:43 +0900

    first commit

---
+++ Dependency.cmake
@@ -0,0 +1,37 @@
+include(ExternalProject)
+
+set(DEP_INSTALL_DIR ${PROJECT_BINARY_DIR}/install)
+set(DEP_INCLUDE_DIR ${DEP_INSTALL_DIR}/include)
+set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
+
+##spdlog로 로그 출력
+
+ExternalProject_Add(
+    dep_spdlog 
+    GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
+    GIT_TAG "v1.x"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+    TEST_COMMAND ""
+)
+
+# Dependency list & library file list
+set(DEP_LIST ${DEP_LIST} dep_spdlog)
+set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:DEBUG>:d>)
+
+ExternalProject_Add(
+    dep_glfw
+    GIT_REPOSITORY "https://github.com/glfw/glfw.git"
+    GIT_TAG "3.3.9"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND "" PATCH_COMMAND "" TEST_COMMAND ""
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+        -DGLFW_BUILD_EXAMPLES=OFF
+        -DGLFW_BUILD_TESTS=OFF
+        -DGLFW_BUILD_DOCS=OFF
+)
+set(DEP_LIST ${DEP_LIST} dep_glfw)
+set(DEP_LIBS ${DEP_LIBS} glfw3)
