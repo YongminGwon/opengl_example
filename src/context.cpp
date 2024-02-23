5575b03e1fc80042134d2ffddac406f88ab261af
arsen <clumsyrhymer@gmail.com>
2024-02-23 11:41:34 +0900

    gradation

--- src/context.cpp
+++ src/context.cpp
@@ -10,20 +10,22 @@ ContextUPtr Context::Create() {
 bool Context::Init() {
 
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+        -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f,
     };
+
     uint32_t indices[] = {
         0, 1, 3,
         1, 2, 3,
     };
 
     m_vertexLayout = VertexLayout::Create();
-    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float)*12);
+    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float)*24);
 
-    m_vertexLayout -> SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+    m_vertexLayout -> SetAttrib(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    m_vertexLayout -> SetAttrib(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, sizeof(float) * 3);
 
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t) * 6);
 
@@ -39,10 +41,6 @@ bool Context::Init() {
         return false;
     SPDLOG_INFO("program id: {}", m_program->Get());
 
-    auto loc = glGetUniformLocation(m_program->Get(), "color");
-    m_program->Use();
-    glUniform4f(loc, 1.0f, 1.0f, 0.0f, 1.0f);
-
     glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
     return true;
 }
@@ -50,13 +48,6 @@ bool Context::Init() {
 void Context::Render(){
     glClear(GL_COLOR_BUFFER_BIT);
 
-    static float time = 0.0f;
-    float t = sinf(time) * 0.5f + 0.5f;
-    auto loc = glGetUniformLocation(m_program->Get(), "color");
-
     m_program->Use();
-    glUniform4f(loc, t*t, 2.0f*t*(1.0f-t), (1.0f-t) * (1.0f-t), 1.0f);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    time += 0.016f;
 }
