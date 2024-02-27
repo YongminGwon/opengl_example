617b3bdfc88e1d152c5f136d2f57d5dabdf9227c
arsen <clumsyrhymer@gmail.com>
2024-02-27 18:49:55 +0900

    object loading

--- src/context.h
+++ src/context.h
@@ -7,6 +7,8 @@
 #include "buffer.h"
 #include "vertex_layout.h"
 #include "texture.h"
+#include "mesh.h"
+#include "model.h"
 
 CLASS_PTR(Context)
 class Context {
@@ -26,9 +28,8 @@ private:
     ProgramUPtr m_program;
     ProgramUPtr m_simpleProgram;
 
-    VertexLayoutUPtr m_vertexLayout;
-    BufferUPtr m_vertexBuffer;
-    BufferUPtr m_indexBuffer;
+    MeshUPtr m_box;
+    ModelUPtr m_model;
 
     TextureUPtr m_texture;
     TextureUPtr m_texture2;
