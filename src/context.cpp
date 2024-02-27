ce4f3b51fca328f11306e52f9de70b0f9e8ba16d
arsen <clumsyrhymer@gmail.com>
2024-02-27 13:30:18 +0900

    spot light

--- src/context.cpp
+++ src/context.cpp
@@ -85,6 +85,8 @@ void Context::Render() {
         };
         if (ImGui::CollapsingHeader("light", ImGuiTreeNodeFlags_DefaultOpen)) {
             ImGui::DragFloat3("1.position", glm::value_ptr(m_light.position), 0.01f);
+            ImGui::DragFloat3("1.direction", glm::value_ptr(m_light.direction), 0.01f);
+            ImGui::DragFloat2("1.cutoff", glm::value_ptr(m_light.cutoff), 0.5f, 0.0f, 180.0f);
             ImGui::DragFloat("1.distance", &m_light.distance, 0.5f, 0.0f, 3000.0f);
             ImGui::ColorEdit3("1.ambient", glm::value_ptr(m_light.ambient));
             ImGui::ColorEdit3("1.diffuse", glm::value_ptr(m_light.diffuse));
@@ -136,6 +138,9 @@ void Context::Render() {
     m_program->Use();
     m_program->SetUniform("viewPos", m_cameraPos);
     m_program->SetUniform("light.position", m_light.position);
+
+    m_program->SetUniform("light.direction", m_light.direction);
+    m_program->SetUniform("light.cutoff", glm::vec2(cosf(glm::radians(m_light.cutoff[0])), cosf(glm::radians(m_light.cutoff[0] + m_light.cutoff[1]))));
     m_program->SetUniform("light.attenuation", GetAttenuationCoeff(m_light.distance));
     m_program->SetUniform("light.ambient", m_light.ambient);
     m_program->SetUniform("light.diffuse", m_light.diffuse);
